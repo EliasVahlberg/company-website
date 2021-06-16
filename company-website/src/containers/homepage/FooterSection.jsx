54a6c08ed82fe6d26ca4460e47976f808244b89c
Hamed Haghjo <hamedhaghjo@hotmail.com>
2021-06-16 16:55:58 +0200

    Test commit

--- company-website/src/containers/homepage/FooterSection.jsx
+++ company-website/src/containers/homepage/FooterSection.jsx
@@ -2,8 +2,5 @@ import React from "react";
 import { Footer } from "../../components/footer";
 
 export function FooterSection() {
-  return
-  <Footer>
-    
-  </Footer>;
+  return <Footer></Footer>;
 }
